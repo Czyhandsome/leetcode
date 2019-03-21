8df0c66c4aa94ecb9840db57e580557df275afb7
caoziyu <czyhandsome@foxmail.com>
2019-03-21 08:05:35 +0800

    reverse k group

---
+++ src/main/java/ink/czyhandsome/demo/revertnodesinkgroup/Solution.java
@@ -0,0 +1,48 @@
+package ink.czyhandsome.demo.revertnodesinkgroup;
+
+import ink.czyhandsome.utilities.ListNode;
+
+import java.util.Stack;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode h = dummy;
+        boolean isFinish = false;
+        Stack<ListNode> stack = new Stack<>();
+        while (!isFinish) {
+            ListNode next = h.next;
+            for (int i = 0; i < k; i++) {
+                if (next == null) {
+                    isFinish = true;
+                    break;
+                } else {
+                    stack.push(next);
+                    next = next.next;
+                }
+            }
+            if (!isFinish) {
+                ListNode current = h;
+                while (!stack.isEmpty()) {
+                    current.next = stack.pop();
+                    current = current.next;
+                }
+                h = current;
+                h.next = next;
+            }
+        }
+        return dummy.next;
+    }
+}
---
+++ src/test/java/ink/czyhandsome/demo/revertnodesinkgroup/SolutionTest.java
@@ -0,0 +1,29 @@
+package ink.czyhandsome.demo.revertnodesinkgroup;
+
+import ink.czyhandsome.utilities.ListNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * $DESC
+ *
+ * @author 曹子钰, 2019-03-20
+ */
+class SolutionTest {
+
+    @Test
+    void reverseKGroup() {
+        Solution solution = new Solution();
+        assertNull(solution.reverseKGroup(null, 2));
+        assertEquals(Arrays.asList(1, 2, 3), solution.reverseKGroup(ListNode.of(1, 2, 3), 4).toList());
+        assertEquals(Arrays.asList(3, 2, 1, 4, 5), solution.reverseKGroup(ListNode.of(1, 2, 3, 4, 5), 3).toList());
+        assertEquals(Arrays.asList(3, 2, 1, 6, 5, 4),
+                solution.reverseKGroup(ListNode.of(1, 2, 3, 4, 5, 6), 3).toList());
+        assertEquals(Arrays.asList(3, 2, 1, 6, 5, 4, 7),
+                solution.reverseKGroup(ListNode.of(1, 2, 3, 4, 5, 6, 7), 3).toList());
+    }
+}
