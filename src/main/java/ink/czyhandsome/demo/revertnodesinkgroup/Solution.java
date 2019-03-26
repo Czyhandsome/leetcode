e848f54d110072722f3295ca17109446b3ca92a6
caoziyu <czyhandsome@foxmail.com>
2019-03-26 08:35:58 +0800

    reverse k group

--- src/main/java/ink/czyhandsome/demo/revertnodesinkgroup/Solution.java
+++ src/main/java/ink/czyhandsome/demo/revertnodesinkgroup/Solution.java
@@ -2,47 +2,54 @@ package ink.czyhandsome.demo.revertnodesinkgroup;
 
 import ink.czyhandsome.utilities.ListNode;
 
-import java.util.Stack;
-
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) { val = x; }
- * }
+ * Revert k-node group
+ *
+ * @author 曹子钰, 2019-03-22
  */
 class Solution {
+
     ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null) {
-            return null;
+        if (k == 1) {
+            return head;
         }
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode h = dummy;
-        boolean isFinish = false;
-        Stack<ListNode> stack = new Stack<>();
-        while (!isFinish) {
-            ListNode next = h.next;
+        while (h != null) {
+            ListNode lo = h;
             for (int i = 0; i < k; i++) {
-                if (next == null) {
-                    isFinish = true;
+                lo = lo.next;
+                if (lo == null) {
                     break;
-                } else {
-                    stack.push(next);
-                    next = next.next;
                 }
             }
-            if (!isFinish) {
-                ListNode current = h;
-                while (!stack.isEmpty()) {
-                    current.next = stack.pop();
-                    current = current.next;
-                }
-                h = current;
-                h.next = next;
+            if (lo == null) {
+                break;
             }
+            h = doRevert(h, lo.next);
         }
         return dummy.next;
     }
+
+    private ListNode doRevert(ListNode hi, ListNode lo) {
+        ListNode f = hi.next;
+        ListNode p = f;
+        ListNode l = p.next;
+        ListNode ln = l.next;
+        while (true) {
+            l.next = p;
+            p = l;
+            if (ln == lo) {
+                break;
+            } else {
+                l = ln;
+                ln = ln.next;
+            }
+        }
+        hi.next = l;
+        f.next = lo;
+        return f;
+    }
+
 }
--- src/test/java/ink/czyhandsome/demo/revertnodesinkgroup/Solution2Test.java
+++ src/test/java/ink/czyhandsome/demo/revertnodesinkgroup/Solution2Test.java
@@ -11,15 +11,14 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 /**
  * $DESC
  *
- * @author 曹子钰, 2019-03-26
+ * @author 曹子钰, 2019-03-20
  */
-class SolutionTest {
+class Solution2Test {
 
     @Test
     void reverseKGroup() {
-        Solution solution = new Solution();
+        Solution2 solution = new Solution2();
         assertNull(solution.reverseKGroup(null, 2));
-        assertEquals(Arrays.asList(1, 2, 3), solution.reverseKGroup(ListNode.of(1, 2, 3), 1).toList());
         assertEquals(Arrays.asList(1, 2, 3), solution.reverseKGroup(ListNode.of(1, 2, 3), 4).toList());
         assertEquals(Arrays.asList(3, 2, 1, 4, 5), solution.reverseKGroup(ListNode.of(1, 2, 3, 4, 5), 3).toList());
         assertEquals(Arrays.asList(3, 2, 1, 6, 5, 4),
@@ -27,5 +26,4 @@ class SolutionTest {
         assertEquals(Arrays.asList(3, 2, 1, 6, 5, 4, 7),
                 solution.reverseKGroup(ListNode.of(1, 2, 3, 4, 5, 6, 7), 3).toList());
     }
-
 }
