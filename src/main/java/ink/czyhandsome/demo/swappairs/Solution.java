05c0b1e8d7ce978e3ca0c54b51eff242db15cd0e
zycao3 <zycao3@iflytek.com>
2020-01-25 17:47:55 +0800

    swappairs/Solution.java

---
+++ src/main/java/ink/czyhandsome/demo/swappairs/Solution.java
@@ -0,0 +1,17 @@
+package ink.czyhandsome.demo.swappairs;
+
+import ink.czyhandsome.utilities.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    ListNode swapPairs(ListNode head) {
+        
+    }
+}
